a52410d03a883cbb1481eec54904ec48b47d60f1
Ricardo Ramirez <ricky.ramirez89@gmail.com>
2024-09-06 10:49:49 -0600

    hmr setup

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as compression from 'compression';
+
+declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors('');
-  // app.useGlobalPipes(new ValidationPipe());
-  // app.use(compression());
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(compression());
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
   await app.listen(3000);
 }
 bootstrap();
